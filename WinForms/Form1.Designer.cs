c26a4f3e2d941d9575f795ca3e0680ea032f467f
Deng Yu <jardy@live.cn>
2020-03-15 15:12:58 +0800

    Web Browser

--- WinForms/Form1.Designer.cs
+++ WinForms/Form1.Designer.cs
@@ -28,63 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnPrevious = new System.Windows.Forms.Button();
-            this.btnNext = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.wbrower = new System.Windows.Forms.WebBrowser();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnAccess = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // wbrower
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(962, 575);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.wbrower.Location = new System.Drawing.Point(12, 48);
+            this.wbrower.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbrower.Name = "wbrower";
+            this.wbrower.Size = new System.Drawing.Size(926, 575);
+            this.wbrower.TabIndex = 0;
             // 
-            // btnPrevious
+            // txtURL
             // 
-            this.btnPrevious.Location = new System.Drawing.Point(221, 603);
-            this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(135, 52);
-            this.btnPrevious.TabIndex = 1;
-            this.btnPrevious.Text = "上一张";
-            this.btnPrevious.UseVisualStyleBackColor = true;
-            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            this.txtURL.Location = new System.Drawing.Point(13, 13);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(833, 21);
+            this.txtURL.TabIndex = 1;
             // 
-            // btnNext
+            // btnAccess
             // 
-            this.btnNext.Location = new System.Drawing.Point(595, 603);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(130, 52);
-            this.btnNext.TabIndex = 2;
-            this.btnNext.Text = "下一张";
-            this.btnNext.UseVisualStyleBackColor = true;
-            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            this.btnAccess.Location = new System.Drawing.Point(852, 12);
+            this.btnAccess.Name = "btnAccess";
+            this.btnAccess.Size = new System.Drawing.Size(75, 23);
+            this.btnAccess.TabIndex = 2;
+            this.btnAccess.Text = "访问";
+            this.btnAccess.UseVisualStyleBackColor = true;
+            this.btnAccess.Click += new System.EventHandler(this.btnAccess_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(968, 667);
-            this.Controls.Add(this.btnNext);
-            this.Controls.Add(this.btnPrevious);
-            this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.ClientSize = new System.Drawing.Size(950, 635);
+            this.Controls.Add(this.btnAccess);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.wbrower);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnPrevious;
-        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.WebBrowser wbrower;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnAccess;
     }
 }
 
--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -18,29 +18,17 @@ namespace WinForms
             InitializeComponent();
         }
 
-        int i = 0;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.Image = Image.FromFile(@"C:\Users\ms\Desktop\5.jpg");            
+          
         }
 
-        //获取文件夹下面所有文件的路径
-        string[] paths = Directory.GetFiles(@"C:\Users\ms\Desktop\");
-
-        private void btnNext_Click(object sender, EventArgs e)
+        private void btnAccess_Click(object sender, EventArgs e)
         {
-            if (i == paths.Length)
-                i = 0;
-            pictureBox1.Image = Image.FromFile(paths[i++]);
-        }
-
-        private void btnPrevious_Click(object sender, EventArgs e)
-        {
-            if (i < 0)
-                i = paths.Length - 1;
-            pictureBox1.Image = Image.FromFile(paths[i--]);
+            //注意：不会给你默认加上http等前缀
+            wbrower.Url = new Uri(@"http://" + txtURL.Text);
+           
         }
     }
 }
