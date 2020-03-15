318f49b96bde402210fa2f33533bab37f3209fef
Deng Yu <jardy@live.cn>
2020-03-15 23:35:48 +0800

    ListBox and SoundPlayer

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,14 +22,16 @@ namespace WinForms
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string[] paths = Directory.GetFiles(@"C:\Users\Administrator\Desktop\MM");
+            string[] paths = Directory.GetFiles(@"C:\Users\Administrator\Desktop\WAV");
             lstPic.Items.AddRange(paths);
         }
 
         private void lstPic_SelectedIndexChanged(object sender, EventArgs e)
         {
             picbDisplay.SizeMode = PictureBoxSizeMode.Zoom;
-            picbDisplay.Image = Image.FromFile(lstPic.SelectedItem.ToString());
+            //picbDisplay.Image = Image.FromFile(lstPic.SelectedItem.ToString());
+            SoundPlayer sp = new SoundPlayer(lstPic.SelectedItem.ToString());
+            sp.Play();
         }
     }
 }
