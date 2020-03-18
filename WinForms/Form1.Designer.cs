19b37f257003c9094f757653735cbd3aaa0c035c
邓宇 <jardy@live.cn>
2020-03-18 11:44:18 +0800

    Thread 线程类

--- WinForms/Form1.Designer.cs
+++ WinForms/Form1.Designer.cs
@@ -30,117 +30,43 @@
         {
             this.components = new System.ComponentModel.Container();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.txtContent = new System.Windows.Forms.TextBox();
-            this.mnuStrip = new System.Windows.Forms.MenuStrip();
-            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.格式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.自动换行ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.样式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.字体ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.颜色ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
-            this.mnuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtContent
+            // button1
             // 
-            this.txtContent.Location = new System.Drawing.Point(0, 28);
-            this.txtContent.Multiline = true;
-            this.txtContent.Name = "txtContent";
-            this.txtContent.Size = new System.Drawing.Size(490, 337);
-            this.txtContent.TabIndex = 3;
-            this.txtContent.WordWrap = false;
+            this.button1.Location = new System.Drawing.Point(93, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // mnuStrip
+            // lblDisplay
             // 
-            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.文件ToolStripMenuItem,
-            this.格式ToolStripMenuItem,
-            this.样式ToolStripMenuItem});
-            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
-            this.mnuStrip.Name = "mnuStrip";
-            this.mnuStrip.Size = new System.Drawing.Size(490, 25);
-            this.mnuStrip.TabIndex = 4;
-            this.mnuStrip.Text = "menuStrip1";
-            // 
-            // 文件ToolStripMenuItem
-            // 
-            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.打开ToolStripMenuItem,
-            this.保存ToolStripMenuItem});
-            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
-            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.文件ToolStripMenuItem.Text = "文件";
-            // 
-            // 打开ToolStripMenuItem
-            // 
-            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
-            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.打开ToolStripMenuItem.Text = "打开";
-            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
-            // 
-            // 保存ToolStripMenuItem
-            // 
-            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
-            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.保存ToolStripMenuItem.Text = "保存";
-            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
-            // 
-            // 格式ToolStripMenuItem
-            // 
-            this.格式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.自动换行ToolStripMenuItem});
-            this.格式ToolStripMenuItem.Name = "格式ToolStripMenuItem";
-            this.格式ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.格式ToolStripMenuItem.Text = "格式";
-            // 
-            // 自动换行ToolStripMenuItem
-            // 
-            this.自动换行ToolStripMenuItem.Name = "自动换行ToolStripMenuItem";
-            this.自动换行ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.自动换行ToolStripMenuItem.Text = "自动换行";
-            this.自动换行ToolStripMenuItem.Click += new System.EventHandler(this.自动换行ToolStripMenuItem_Click);
-            // 
-            // 样式ToolStripMenuItem
-            // 
-            this.样式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.字体ToolStripMenuItem,
-            this.颜色ToolStripMenuItem});
-            this.样式ToolStripMenuItem.Name = "样式ToolStripMenuItem";
-            this.样式ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.样式ToolStripMenuItem.Text = "样式";
-            // 
-            // 字体ToolStripMenuItem
-            // 
-            this.字体ToolStripMenuItem.Name = "字体ToolStripMenuItem";
-            this.字体ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.字体ToolStripMenuItem.Text = "字体";
-            this.字体ToolStripMenuItem.Click += new System.EventHandler(this.字体ToolStripMenuItem_Click);
-            // 
-            // 颜色ToolStripMenuItem
-            // 
-            this.颜色ToolStripMenuItem.Name = "颜色ToolStripMenuItem";
-            this.颜色ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.颜色ToolStripMenuItem.Text = "颜色";
-            this.颜色ToolStripMenuItem.Click += new System.EventHandler(this.颜色ToolStripMenuItem_Click);
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(91, 167);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(41, 12);
+            this.lblDisplay.TabIndex = 1;
+            this.lblDisplay.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(490, 366);
-            this.Controls.Add(this.txtContent);
-            this.Controls.Add(this.mnuStrip);
-            this.MainMenuStrip = this.mnuStrip;
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "小型记事本";
+            this.Text = "多线程";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
-            this.mnuStrip.ResumeLayout(false);
-            this.mnuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,16 +74,8 @@
 
         #endregion
         private System.Windows.Forms.BindingSource bindingSource1;
-        private System.Windows.Forms.TextBox txtContent;
-        private System.Windows.Forms.MenuStrip mnuStrip;
-        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 格式ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 自动换行ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 样式ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 字体ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 颜色ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDisplay;
     }
 }
 
--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,56 +19,42 @@ namespace WinForms
         {
             InitializeComponent();
         }
-        public string path;
+
+        Thread th;
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //取消跨线程访问(非常不建议这样做)
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //前台线程：只有所有前台线程都关闭才算程序关闭
+            //后台线程：只有前台线程结束后，后台线程自动结束
+            //创建一个线程去执行该方法
+            th = new Thread(Test);
+            //标记这个线程准备就绪了，可以随时被执行（具体由CPU执行）
+            th.IsBackground = true; //设置成后台线程
+            th.Start();
             
         }
 
-        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void Test()
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.InitialDirectory = @"C:\Users\Administrator\Desktop";
-            ofd.Filter = "文本文件|*.txt";
-            ofd.ShowDialog();
-            path = ofd.FileName;
-            string line;
-            StreamReader sr = new StreamReader(path);
-            while ((line = sr.ReadLine()) != null)
-                txtContent.AppendText(line);
+            for (int i = 0; i < 200000; i++)
+            {
+                //Console.WriteLine(i);
+                lblDisplay.Text = i.ToString();
+            }
         }
-
-        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            StreamWriter sw = new StreamWriter(path);
-            sw.Write(txtContent.Text);
-            sw.Close();
-        }
-
-        private void 自动换行ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            txtContent.WordWrap = !txtContent.WordWrap;
-        }
-
-        private void 字体ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            FontDialog fd = new FontDialog();
-            fd.ShowDialog();
-            txtContent.Font = fd.Font;
-        }
-
-        private void 颜色ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ColorDialog cd = new ColorDialog();
-            cd.ShowDialog();
-            txtContent.ForeColor = cd.Color;
-        }
-
-
         
-
-        
-
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (th != null)
+            {
+                th.Abort();//手动结束这个线程
+            }
+        }
     }
 }
